09cb29eebbf4898dc80a8d47c212ebabbdf286d7
benjamin <benjamin1990@163.com>
2015-12-08 19:15:01 +0800

    fix bugs

--- src/main/java/com/benjamin/websocket/InternalRequestController.java
+++ src/main/java/com/benjamin/websocket/InternalRequestController.java
@@ -2,17 +2,23 @@ package com.benjamin.websocket;
 
 
 import com.benjamin.websocket.entity.StringIdentity;
+import com.benjamin.websocket.sessionprovider.WebSocketSessionProvider;
+import com.benjamin.websocket.sessionprovider.factory.impl.DefaultLocalSessionProviderFactory;
+import com.benjamin.websocket.sessionprovider.factory.impl.DefaultWebSocketSessionProviderFactory;
 import com.benjamin.websocket.sessionprovider.impl.SimpWebSocketSessionProvider;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import java.io.IOException;
 
 /**
@@ -23,15 +29,24 @@ public class InternalRequestController {
 
   private static Logger logger = Logger.getLogger(InternalRequestController.class);
 
-  @Autowired
-  private SimpWebSocketSessionProvider simpWebSocketSessionProvider;
+  @Resource(name = "defaultWebSocketSessionProviderFactory")
+  private DefaultWebSocketSessionProviderFactory defaultWebSocketSessionProviderFactory;
+
+  private WebSocketSessionProvider<WebSocketSession> simpWebSocketSessionProvider;
+
+  @PostConstruct
+  private void postConstruct(){
+    logger.debug("POST construct ---- set simpleWebSocketSessionProvider ");
+    simpWebSocketSessionProvider = defaultWebSocketSessionProviderFactory.getWebSocketSessionProvider(false);
+  }
 
   @RequestMapping("/internalRequest")
   @ResponseBody
   public JSONObject handleInternalRequest(@RequestParam("mac") String identify, @RequestParam("message") String message){
     JSONObject result = new JSONObject();
     WebSocketSession webSocketSession = simpWebSocketSessionProvider.getSession(new StringIdentity(identify));
-
+    Assert.notNull(webSocketSession);
+    Assert.isTrue(webSocketSession.isOpen());
     if(webSocketSession !=null && webSocketSession.isOpen()){
       if(logger.isDebugEnabled()){
         logger.debug("处理请求的WebSocketSessionId为#######"+webSocketSession.getId());
